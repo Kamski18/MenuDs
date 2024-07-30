dd10e2a07475e103efa35966135285798889e2be
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-07-31 00:46:47 +0800

    Make the bot use Malaysia's timezone, and make sure to refresh the date

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import telebot
-import datetime
 import os
 from dotenv import load_dotenv
+from pytz import timezone
+from datetime import datetime
 
 
-today = datetime.datetime.now()
+tz = timezone("Asia/Kuala_Lumpur")
+today = datetime.now(tz)
 date = today.strftime("%d")
 day = today.strftime("%d / %B / %Y")
 
@@ -12,6 +14,12 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
+
+def refresh_date():
+  global today, date, day
+  today = datetime.now(tz)
+  date = today.strftime("%d")
+  day = today.strftime("%d / %B / %Y")
 
 
 @bot.message_handler(commands=['start'])
@@ -21,18 +29,19 @@ def start(message):
 
 @bot.message_handler(commands=['menu'])
 def menu(message):
+  refresh_date()
   send = f'menus/{date}.png'
-  if date == date:
-    try:
-      with open(send, 'rb') as photo:
-       bot.send_photo(message.chat.id, photo)
-       bot.send_message(message.chat.id, f"This is the menu for : {day}")
-      
-    except FileNotFoundError:
-      bot.send_message(message.chat.id, "No photo 'yet'..")
+  try:
+    with open(send, 'rb') as photo:
+      bot.send_photo(message.chat.id, photo)
+      bot.send_message(message.chat.id, f"This is the menu for : {day}")
+    
+  except FileNotFoundError:
+    bot.send_message(message.chat.id, "No photo 'yet'..")
 
 @bot.message_handler(commands=['tmr'])
 def tmr(message):
+  refresh_date()
   n = int(date) + 1
   send = f'menus/{n}.png'
   try:
