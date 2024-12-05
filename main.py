f32e56d6aadda4a7b451e673dd7eca81329bf4a2
Kamski18 <151996410+Kamski18@users.noreply.github.com>
2024-12-06 07:43:17 +0800

    Strip 0

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ date = today.strftime("%d")
 day = today.strftime("%d / %B / %Y")
 
 tmr = today + timedelta(days=1)
-tmr = tmr.strftime("%d")
+tmr = tmr.strftime("%d").lstrip("0")
 
 
 def create_keyboard():
